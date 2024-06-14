0eafc1e56f70039a8aeeaf027222a5b67f631c52
machearn <machearn.n@gmail.com>
2024-06-14 18:42:11 -0400

    feat: add all in one include file

---
+++ include/coro.hpp
@@ -0,0 +1,15 @@
+#ifndef CORO_HPP
+#define CORO_HPP
+
+#include <concepts/awaitable.hpp>
+#include <concepts/executor.hpp>
+
+#include <libcoro/event.hpp>
+#include <libcoro/event_fd.hpp>
+#include <libcoro/file.hpp>
+#include <libcoro/generator.hpp>
+#include <libcoro/io_service.hpp>
+#include <libcoro/latch.hpp>
+#include <libcoro/task.hpp>
+
+#endif // !CORO_HPP
